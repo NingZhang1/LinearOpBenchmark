80bc5b3ff70416c11b5e91d2cb2fecd9b6da47e3
NingZhang <ningzhang1024@gmail.com>
2024-01-12 21:12:48 -0800

    update 2024.01.12 add eigen hello world

---
+++ utils/config.h
@@ -0,0 +1,132 @@
+/*
+ * @Author: Ning Zhang
+ * @Date: 2024-01-12 20:54:47
+ * @Last Modified by: Ning Zhang
+ * @Last Modified time: 2024-01-12 20:59:20
+ */
+
+#ifndef CONFIG_H
+#define CONFIG_H
+
+/* C lib */
+
+#include <stdint.h>
+#include <omp.h>
+#include <stdio.h>
+#include <string.h>
+#include <cmath>
+#include <cstdlib>
+#include <stddef.h>
+#include <unistd.h>
+#include <ctype.h>
+#include <sys/mman.h> /* mmap munmap */
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <assert.h>
+#include <execinfo.h>
+
+/* CXX Lib */
+
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <vector>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <array>
+#include <set>
+#include <algorithm>
+#include <functional>
+#include <iterator>
+#include <numeric>
+#include <regex>
+#include <stdexcept>
+#include <complex>
+#include <chrono>
+#include <map>
+#include <type_traits>
+#include <memory>
+#include <random>
+
+/* 多线程相关 */
+
+#define OMP_NUM_OF_THREADS std::atoi(getenv("OMP_NUM_THREADS"))
+#define OMP_THREAD_LABEL omp_get_thread_num()
+
+/* math lib */
+
+#ifdef _MKL_
+#include <mkl.h>
+#else
+
+#include <cblas.h>
+#include <lapacke.h>
+
+#define MKL_INT CBLAS_INDEX
+#define MKL_Complex16 std::complex<double>
+#define MKL_Complex8 std::complex<float>
+
+inline void cblas_daxpyi(const MKL_INT N, const double alpha, const double *X,
+                         const MKL_INT *indx, double *Y)
+{
+    for (MKL_INT i = 0; i < N; ++i)
+    {
+        Y[indx[i]] += alpha * X[i];
+    }
+}
+
+inline double cblas_ddoti(const MKL_INT N, const double *X, const MKL_INT *indx,
+                          const double *Y)
+{
+    double res = 0.0;
+    for (MKL_INT i = 0; i < N; ++i)
+    {
+        res += X[i] * Y[indx[i]];
+    }
+    return res;
+}
+
+// #define CHECK_DSCRMV
+
+inline void mkl_dcsrmv(const char *transa, const MKL_INT *m, const MKL_INT *k, const double *alpha, const char *matdescra, const double *val,
+                       const MKL_INT *indx, const MKL_INT *pntrb, const MKL_INT *pntre, const double *x, const double *beta, double *y)
+{
+    for (auto irow = 0; irow < *m; ++irow)
+    {
+        double tmp = 0.0;
+        for (auto icol = pntrb[irow]; icol < pntre[irow]; ++icol)
+        {
+            tmp += val[icol] * x[indx[icol]];
+        }
+        y[irow] = tmp;
+    }
+}
+
+inline void mkl_zcsrmv(const char *transa, const MKL_INT *m, const MKL_INT *k, const MKL_Complex16 *alpha, const char *matdescra, const MKL_Complex16 *val,
+                       const MKL_INT *indx, const MKL_INT *pntrb, const MKL_INT *pntre, const MKL_Complex16 *x, const MKL_Complex16 *beta, MKL_Complex16 *y)
+{
+    for (auto irow = 0; irow < *m; ++irow)
+    {
+        MKL_Complex16 tmp = 0.0;
+        for (auto icol = pntrb[irow]; icol < pntre[irow]; ++icol)
+        {
+            tmp += val[icol] * x[indx[icol]];
+        }
+        y[irow] = tmp;
+    }
+}
+
+inline void cblas_dsctr(const MKL_INT N, const double *X, const MKL_INT *indx,
+                        double *Y)
+{
+    for (MKL_INT i = 0; i < N; ++i)
+    {
+        Y[indx[i]] = X[i];
+    }
+}
+
+#endif
+
+#endif // CONFIG_H
---
+++ utils/random_generator.h
@@ -0,0 +1,80 @@
+#ifndef RANDOM_GENERATOR_H
+#define RANDOM_GENERATOR_H
+
+#include "config.h"
+
+/* >>>>>>>>>>>>>>>>>>> generate random number <<<<<<<<<<<<<<<<<<<<<< */
+
+template <typename Scalar>
+Scalar GenerateRandomNumber(Scalar _Input);
+
+template <>
+inline double GenerateRandomNumber<double>(double _Input)
+{
+    return (rand() / (double)(RAND_MAX)-0.5);
+}
+
+template <>
+inline std::complex<double> GenerateRandomNumber<std::complex<double>>(std::complex<double> _input)
+{
+    std::complex<double> Res;
+    Res.real(rand() / (double)(RAND_MAX)-0.5);
+    Res.imag(rand() / (double)(RAND_MAX)-0.5);
+    return Res;
+}
+
+inline std::vector<double> GenerateRandomDoubleVector(const int _nelmt)
+{
+    srand(time(NULL));
+    std::vector<double> Res;
+    for (int i = 0; i < _nelmt; ++i)
+    {
+        Res.push_back(GenerateRandomNumber<double>(0.0));
+    }
+    return Res;
+}
+
+inline std::vector<double> GenerateRandomMatrix(const int _ndim)
+{
+    return GenerateRandomDoubleVector(_ndim * _ndim);
+}
+
+inline std::vector<double> GenerateRandomMatrix(const int _nrow, const int _ncol)
+{
+    return GenerateRandomDoubleVector(_nrow * _ncol);
+}
+
+/* >>>>>>>>>>>>>>>>>>> SparseMat Generator <<<<<<<<<<<<<<<<<<<<<< */
+
+typedef std::pair<std::vector<double>, std::vector<MKL_INT>> ColIndxMat;
+
+std::vector<MKL_INT> GenerateRandomIndx(const int _DIM, const int _NELMT)
+{
+    srand(time(NULL));
+    std::vector<MKL_INT> Res;
+    for (int i = 0; i < _NELMT; ++i)
+    {
+        Res.push_back(rand() % _DIM);
+    }
+    std::sort(Res.begin(), Res.end());
+    Res.erase(std::unique(Res.begin(), Res.end()), Res.end());
+    return Res;
+}
+
+ColIndxMat GenerateRandomSparseVector(const int _DIM, const double _DENSITY, const bool _ORDERED)
+{
+    assert(_DENSITY > 0 - 1e-10);
+    assert(_DENSITY < 1 - 1e-10);
+    const int NELMT = _DIM * _DENSITY + 1;
+    auto Indx = GenerateRandomIndx(_DIM, NELMT);
+    auto NELMT_REAL = Indx.size();
+    if (!_ORDERED)
+    {
+        std::random_device rd;
+        std::mt19937 rng(rd());
+        std::shuffle(Indx.begin(), Indx.end(), rng);
+    }
+    return ColIndxMat(GenerateRandomDoubleVector(Indx.size()), Indx);
+}
+
+#endif // !RANDOM_GENERATOR_H
